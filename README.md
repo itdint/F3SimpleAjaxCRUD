c7474e2e0b341d2446778fe1fc1b411f51aa6e20
itdint <itdint@gmail.com>
2015-06-15 13:04:48 -0400

    cleaning up

--- README.md
+++ README.md
@@ -11,3 +11,5 @@ This little sample was created to run on a local test setup (MAMP, WAMP, etc..)
 
 #nginx notes
 This sample app has a .htaccess file and is intended to run under apache server.  However, if you are using a server with nginx - you just have to make sure you have your conf file set up correctly.  There is lots of information out there about this - just google "f3 nginx configuration" or something similar.
+
+Hope this helps!
--- tmp/1vgk5solz87wt.laaa7rm999i6.php
+++
@@ -1,9 +0,0 @@
-    </div>
-  <!-- Placed at the end of the document so the pages load faster -->
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-  <!-- Latest compiled and minified JavaScript -->
-  <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-  <!-- js code for Ajax functions.  -->
-  <script type="text/javascript" language="javascript" src="../../ui/js/ajaxContacts.js"></script>
-  </body>
-</html>
